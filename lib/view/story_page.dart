13c5fb948e41ca6b48d714f832c0aa47701cd4e9
Mohamed28102002 <mohamedwalid28102002@gmail.com>
2024-11-30 17:43:29 +0200

    create list page and story page

---
+++ lib/view/story_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StoryPage extends StatelessWidget {
+  const StoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
