4b07bfa03c3c03e1914c3e2e9cfc2c9f637eaa61
Mohamed28102002 <mohamedwalid28102002@gmail.com>
2024-11-30 17:23:53 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(bigChat());
+}
+
+class bigChat extends StatelessWidget {
+  const bigChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
